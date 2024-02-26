f0b02ec17d1364fcb643824130a10edceaa1343a
n335t3r <n335t3r@gmail.com>
2024-02-26 13:12:21 -0500

    home in good spot (for now)

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,33 @@
+import populateAbout from "./about";
+
 export default function populateHome() {
     const content = document.getElementById('content');
     content.innerHTML = "";
 
     const live = document.createElement('h1');
+    live.classList.add('live');
     live.textContent = "We're LIVE!";
     content.appendChild(live);
+
+    const more = document.createElement('h4');
+    more.classList.add('more');
+    more.textContent = "and more is coming (including some blog content)!";
+    content.appendChild(more);
+
+    const checkAbout = document.createElement('div');
+    checkAbout.classList.add('checkAbout');
+    content.appendChild(checkAbout);
+
+    const aboutLink = document.createElement('h3');
+    aboutLink.classList.add('aboutLink');
+    aboutLink.textContent = "Click HERE";
+    aboutLink.onclick = () => {
+        populateAbout();
+    };
+    checkAbout.appendChild(aboutLink);
+
+    const aboutTag = document.createElement('h4');
+    aboutTag.classList.add('aboutTag');
+    aboutTag.textContent = "to learn more about your maitre d'!";
+    checkAbout.appendChild(aboutTag);
 }
