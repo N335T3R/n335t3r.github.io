0a4f96ea7f21919c81fbe71078cc633b1935958d
n335t3r <n335t3r@gmail.com>
2024-02-25 21:03:19 -0500

    header complete with profile photo, nametag content (& cypher functionality), & nav

--- src/index.js
+++ src/index.js
@@ -1,19 +1,49 @@
 import "./styles.css";
+import Profile from "./assets/n335t3rProfile.png";
 import makeTable from "./utils/headerUtils";
 import { encode } from "./utils/headerUtils";
 
 
+
+
+// HEADER
+const header = document.getElementById('header');
+// profile picture
+const profContainer = document.getElementById('profContainer');
+const profPic = new Image();
+profPic.src = Profile;
+profContainer.appendChild(profPic);
+
+
+// n335t3r name
 const nametag = document.getElementById('nametag');
 nametag.textContent = "n335t3r";
+const anthony = document.getElementById('anthony');
+// Vigenere Cypher functionality
+header.onmouseenter = () => {
+    var neeCipher = encode("neester");
+    var antCipher = encode("anthonyhorner");
 
-const test = document.getElementById('test');
-
-
-nametag.onmouseenter = () => {
     nametag.textContent = "";
-    var text = encode("neester");
-    nametag.textContent = text;
+    nametag.textContent = neeCipher;
+
+    anthony.textContent = "";
+    anthony.textContent = antCipher;
 };
-nametag.onmouseleave = () => {
+header.onmouseleave = () => {
     nametag.textContent = "n335t3r";
+    anthony.textContent = "Anthony Horner";
 };
+// END HEADER
+
+
+
+// CONTENT
+
+// END CONTENT
+
+
+
+// SOCIALS
+
+// END SOCIALS
