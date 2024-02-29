6aaf4331b9aeceda5fba620164c5a3344958453c
n335t3r <n335t3r@gmail.com>
2024-02-29 15:58:19 -0500

    header added to sizeAdjust functionality; rebuild

--- src/utils/sizeAdjust.js
+++ src/utils/sizeAdjust.js
@@ -1,8 +1,10 @@
 export default function sizeAdjust() {
+    const header = document.getElementById('header');
     const content = document.getElementById('content');
     const socials = document.getElementById('socials');
 
     if (screen.width < 400) {
+        header.width = 300;
         content.width = 300;
         socials.width = 300;
     }
