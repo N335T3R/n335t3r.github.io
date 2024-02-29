3300806e0bc716b82f1a3f7cfb0a43d878370af8
n335t3r <n335t3r@gmail.com>
2024-02-29 15:03:25 -0500

    css and JS footer size adjustments for smaller screens

--- src/utils/sizeAdjust.js
+++ src/utils/sizeAdjust.js
@@ -1,7 +1,9 @@
 export default function sizeAdjust() {
     const content = document.getElementById('content');
+    const socials = document.getElementById('socials');
 
     if (screen.width < 400) {
         content.width = 300;
+        socials.width = 300;
     }
 }
